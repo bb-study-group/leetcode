75cc6dc554ab858f3c622f8ac75224bf9c0b3a28
Chengyao Liu <brian-lcy@hotmail.com>
2018-12-02 17:57:20 -0500

    first commit

---
+++ lets_have_some_fun/1.two-sum.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=1 lang=python
+#
+# [1] Two Sum
+#
+# https://leetcode.com/problems/two-sum/description/
+#
+# algorithms
+# Easy (39.33%)
+# Total Accepted:    1.2M
+# Total Submissions: 3.1M
+# Testcase Example:  '[2,7,11,15]\n9'
+#
+# Given an array of integers, return indices of the two numbers such that they
+# add up to a specific target.
+# 
+# You may assume that each input would have exactly one solution, and you may
+# not use the same element twice.
+# 
+# Example:
+# 
+# 
+# Given nums = [2, 7, 11, 15], target = 9,
+# 
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+# 
+# 
+# 
+# 
+#
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i, num in enumerate(nums):
+            if (target-num) in dict:
+                return [dict[target-num], i]
+            dict[num] = i
+        return
